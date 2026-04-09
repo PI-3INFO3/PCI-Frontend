5546b8293c616374ea2ab2da77e0fd75e83e9dfe
andersonrsanjo@gmail.com <andersonrsanjo@gmail.com>
2026-04-09 15:02:15 -0300

    feat: estrutura do header componets

---
+++ src/views/HomeViews.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <HeaderComponent />
+    <main class="main-content">
+      <!-- Aqui vão os cards dos templates -->
+    </main>
+  </div>
+</template>
+
+<script setup>
+import HeaderComponent from '../components/HeaderComponet.vue'
+</script>
