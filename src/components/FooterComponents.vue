e2655f30c9bf1ee943ebecc7da16ab5d7e741ac1
Andersonramos2023 <andersonrsanjo@gmail.com>
2026-04-17 11:14:22 -0300

    FEAT(footer-2): Foi colocado os icons, alterado o css e feito o footer. Quase pronto

--- src/components/FooterComponents.vue
+++ src/components/FooterComponents.vue
@@ -1,13 +1,56 @@
 <template>
   <footer class="navbar">
     <nav>
-    <ul>
-        
-    </ul>
-  </nav>
+      <ul>
+        <li>
+          <span
+            class="btn"
+            :class="{ active: ativo === 'menu' }"
+            @click="ativo = 'menu'"
+          >
+            <ion-icon name="menu"></ion-icon>
+          </span>
+        </li>
 
+        <li>
+          <span
+            class="btn"
+            :class="{ active: ativo === 'star' }"
+            @click="ativo = 'star'"
+          >
+            <ion-icon name="star-outline"></ion-icon>
+          </span>
+        </li>
+
+        <li>
+          <span
+            class="btn"
+            :class="{ active: ativo === 'folder' }"
+            @click="ativo = 'folder'"
+          >
+            <ion-icon name="folder-open-outline"></ion-icon>
+          </span>
+        </li>
+
+        <li>
+          <span
+            class="btn"
+            :class="{ active: ativo === 'grid' }"
+            @click="ativo = 'grid'"
+          >
+            <ion-icon name="grid-outline"></ion-icon>
+          </span>
+        </li>
+      </ul>
+    </nav>
   </footer>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const ativo = ref('menu') 
 </script>
+
+<style>
+</style>
