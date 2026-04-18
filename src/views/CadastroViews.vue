b1e3a4df82f425669de9e807c63ab1ef4b57daaf
andersonrsanjo@gmail.com <andersonrsanjo@gmail.com>
2026-04-18 14:50:02 -0300

    FEAT(cadastro-Feito)

---
+++ src/views/CadastroViews.vue
@@ -0,0 +1,143 @@
+<script setup>
+import { ref } from 'vue';
+const ativo = ref('up');
+
+</script>
+<template>
+  <img src="/public/logo-96x96.png" alt="logo">
+
+  <form>
+    <legend><span>Cadastre-se</span></legend>
+
+    <div class="input-box"
+    :class="{active: ativo === 'Nome'}"
+    @click=" ativo = 'Nome'"
+    >
+      <ion-icon name="person-outline"></ion-icon>
+      <input type="text" placeholder="Nome" required>
+    </div>
+
+    <div class="input-box"
+    
+    :class="{active: ativo === 'Nome de usuário'}"
+    @click=" ativo = 'Nome de usuário'"
+    >
+      <ion-icon name="person-circle-outline"></ion-icon>
+      <input type="text" placeholder="Nome de usuário" required>
+    </div>
+
+    <div class="input-box"
+    
+    :class="{active: ativo === 'Email'}"
+    @click=" ativo = 'Email'">
+      <ion-icon name="mail-outline"></ion-icon>
+      <input type="email" placeholder="Email" required>
+    </div>
+
+    <div class="input-box"
+    
+    :class="{active: ativo === 'Senha'}"
+    @click=" ativo = 'Senha'">
+      <ion-icon name="lock-closed-outline"></ion-icon>
+      <input type="password" placeholder="Senha" required>
+    </div>
+
+    <div class="input-box"
+    
+    :class="{active: ativo === 'Confirmar senha'}"
+    @click=" ativo = 'Confirmar senha'"
+    >
+      <ion-icon name="eye-off-outline"></ion-icon>
+      <input type="password" placeholder="Confirmar senha" required>
+    </div>
+    <button type="submit">Cadastrar?</button>
+</form>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+*{
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+img {
+  display: block;
+  margin: 20px auto;
+  margin-top: -5px;
+ filter:  
+ drop-shadow(0 6px 1px rgba(0,0,0,0.10))
+drop-shadow(0 0 4px rgba(0,0,0,0.1));
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+}
+
+legend {
+  font-size: 42px;
+  font-weight: 800;
+  color: #FF5700;
+}
+span{
+    border: none;
+    border: transparent;
+    text-shadow: 0px 5px 5px rgba(0,0,0,.30);
+}
+/* INPUT + ÍCONE */
+.input-box {
+  position: relative;
+  width: 260px;
+  border-radius: 20px; 
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+  transition: transform .3s ease;
+}
+.input-box.active{ 
+transform: scale(1.1);
+box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+}
+
+.input-box input {
+  width: 100%;
+  padding: 12px 20px 12px 45px;
+
+  border-radius: 40px;
+  border: 2px solid #FF5700;
+
+  font-size: 14px;
+  outline: none;
+}
+
+/* ÍCONE */
+.input-box ion-icon {
+  position: absolute;
+  left: 15px;
+  top: 50%;
+  transform: translateY(-50%);
+
+  font-size: 18px;
+  color: #000;
+}
+
+/* BOTÃO */
+button {
+  padding: 10px 80px;
+  border-radius: 40px;
+  background-color: #FF5700;
+  border: none;
+  color: white;
+  font-size: 18px;
+  cursor: pointer;
+  transition:  transform 10s ease-in-out;
+
+}
+button:active{
+transform: scale(1.1);
+}
+</style>
