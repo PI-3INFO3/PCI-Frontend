dbf9a01c2735b5355793ec18fe4404f47941bbf3
diakauankl <kauanluciano673@gmail.com>
2026-05-15 15:01:44 -0300

    user: Criação do conteúdo e css

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -4,28 +4,28 @@
     <h1>Conta</h1>
 
    <button class="user">
-    <img src="" alt="Foto do usuário">
+    <img src="" alt="">
 
     <div class="info">
-        <span class="nome">Nome do Usuário</span> <br>
-        <span class="email">E-mail</span>
+        <span class="nome">Anderson Ramos Silva Anjo</span> <br>
+        <span class="email">Andersonrsanjo@gmail.com</span>
     </div>
 </button>
 
-    <button>Adicionar nova conta</button>
+    <button class="adicionar">Adicionar nova conta</button>
 
     </div>
 
     <div class="opcoes">
         <ul>
             <li>
-               configações
+                <ion-icon name="settings-outline"></ion-icon>    configações
             </li>
             <li>
-                Tema
+                <ion-icon name="sunny-outline"></ion-icon>    Tema <span class="tema">claro </span>
             </li>
             <li>
-                Sair da conta
+                <ion-icon name="log-out-outline"></ion-icon>    Sair da conta 
             </li>
         </ul>
     </div>
@@ -35,30 +35,55 @@
 .conta {
     display: flex;
     flex-direction: column;
-    align-items: center;
-}
-div.conta {
-    display: flex;
-    gap: 10px 0 0 0 ;   
+    gap: 25px;
 }
 button.user {
     display: flex;
     background-color: white;
     border: 2px solid black;
     border-radius: 7px;
+    padding : 10px;
+}
+span {
+    margin: 10vw;
+}
+.nome {
+    font-size: 15px;
+    font-weight: bold;
+}
+.email {
+    font-size: 13px;
+    font-weight: normal;
+}
+.adicionar {
+    background-color: white;
+    border: 2px solid black;
+    border-radius: 7px;
+    padding: 5px;
+    margin: 0 85px 0 85px;
+}
+.opcoes {
+    display: flex;
+    flex-direction: column;
+    margin: 43px 0 0 0;
 }
 ul {
     display: flex;
     flex-direction: column;
+    gap: 48px;
     list-style-type: none;
 }
-li.nome {
-    font-size: 20.3px;
-    font-weight: bold;
+li {
+    font-size: 17px;
+    display: flex;
+    align-items: center;
 }
-li.email {
-    font-size: 15px;
-    font-weight: normal;
+.tema {
+    margin-left: 80px;
+}
+ion-icon {
+    font-size: 20px;
+    margin-right:30px;
 }
 
 </style>
