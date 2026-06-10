82592b2f91c9b71f2b022bf0d188830292ff5027
andersonramos2023 <andersonrsanjo@gmail.com>
2026-06-10 09:45:01 -0300

    fix(dev): import duplicado removido

--- src/main.js
+++ src/main.js
@@ -18,7 +18,6 @@ import { createPinia } from 'pinia';
 import './assets/css/style.css'
 import router from './router'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App);
 app.use(createPinia());
