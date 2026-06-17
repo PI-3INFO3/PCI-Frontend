3a948b3594c9de2e5acb000eb264cd54e8f0d692
Milenakindleindarosa <milena.kindlein.da.da.rosa.ifc@gmail.com>
2026-06-17 10:34:04 -0300

    feat: importando os icones

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import './assets/css/style.css'
 import router from './router'
 import App from './App.vue'
 
+import { defineCustomElements } from 'ionicons/loader'
+
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
+
+defineCustomElements(window)
+
 app.mount('#app');
